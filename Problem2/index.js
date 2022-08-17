ecd0dff6dd5ad916c6f52313e9ab671a43601e02
Manisha1299 <manishamore1299@gmail.com>
2022-08-17 18:50:40 +0530

    deployed

---
+++ Problem2/index.js
@@ -0,0 +1,12 @@
+const data = require("./student/getDetails.json");
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 1010
+app.get("/",(req,res)=>{
+    res.send("api is running..");
+})
+
+app.post("/student/getDetails", function (req, res) {
+    res.send(data);
+});
+app.listen(port, console.log(`server runnig..... `));
